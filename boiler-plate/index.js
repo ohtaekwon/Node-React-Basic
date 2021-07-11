2dfc744b295eea0bbcc212f09d4e02ca46fa6647
Tae KWon <ohtk1119@gamil.com>
2021-07-12 02:42:34 +0900

    21.07.12 03-04 learning : 03 MongoDB  04 Model & Schema

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,8 +2,17 @@ const express = require('express') // 설치한 express 모듈 가져온다.
 const app = express()  // 함수로 가져옴
 const port = 3000 // 3000 포트를 백서버로 둔다.
 
+
+// mongodb+srv://TKOH:<password>@boilerplate.vil4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://TKOH:1234@boilerplate.vil4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false //에러를 방지하기 위해
+}).then(()=>console.log('MongoDB Connected....'))
+  .catch(err=> console.log(err))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!~~~ 무야호!!!')
+  res.send('Hello World!~~~ 안녕 무야호~~!!')
 })
 
 app.listen(port, () => {
